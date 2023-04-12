77f7cc752e20ad523615e8a381ed2c29967a22a4
jKy0n <jk.1989@hotmail.com>
2023-04-12 08:04:20 -0300

    Add dwmBlocks

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -3,3 +3,5 @@ picom --daemon &
 dwmblocks &
 sleep 1 && /usr/libexec/polkit-gnome-authentication-agent-1 &
 feh --bg-fill --no-xinerama ~/Pictures/Wallpapers/LinuxWallpapers/multi-monitor-wallpapers.jpg
+
+while type dwm >/dev/null; do dwm && continue || break; done
