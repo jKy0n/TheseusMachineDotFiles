cfd5e54dba20ae5095c4779e3b0e096603c0dffb
jKy0n <jk.1989@hotmail.com>
2023-04-21 10:55:14 -0300

    dwm/dmenu: ~ font, rules | dwmBlks + nice | picom

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-picom --daemon &
-dwmblocks &
-sleep 1 && /usr/libexec/polkit-gnome-authentication-agent-1 &
 feh --bg-fill --no-xinerama ~/Pictures/Wallpapers/LinuxWallpapers/multi-monitor-wallpapers.jpg
+picom --daemon &
+nice -n 15 dwmblocks &
+sleep 1 && /usr/libexec/polkit-gnome-authentication-agent-1 &
